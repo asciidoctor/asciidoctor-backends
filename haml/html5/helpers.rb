409a59c36e9aff42a400ba324aaa558ebf580d53
Alex Soto <asotobu@gmail.com>
2013-02-20 19:34:51 +0100

    Updated file structure.

---
+++ haml/html5/helpers.rb
@@ -0,0 +1,7 @@
+# Add custom functions to this module that you want to use in your Haml
+# templates. Within the template you can invoke them as top-level functions
+# just like the built-in helper functions that Haml provides.
+module Haml::Helpers
+  #def a_helper_function
+  #end
+end
