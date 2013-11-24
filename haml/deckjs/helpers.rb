06e04a59f19a21603c38b7e5fd0326563fba04ed
Dan Allen <dan.j.allen@gmail.com>
2013-11-24 13:06:11 -0700

    import the stylesheet functions for the syntax highlighters

---
+++ haml/deckjs/helpers.rb
@@ -0,0 +1,8 @@
+require 'asciidoctor/backends/_stylesheets'
+# Add custom functions to this module that you want to use in your Haml
+# templates. Within the template you can invoke them as top-level functions
+# just like the built-in helper functions that Haml provides.
+# module Haml::Helpers
+#   def a_helper_function
+#   end
+# end
