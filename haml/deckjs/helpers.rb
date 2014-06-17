75bb791bc06f132ab46d67f98d5370f0ed69f098
Dan Allen <dan.j.allen@gmail.com>
2014-06-17 15:10:46 -0600

    update backends for Asciidoctor 1.5.0 preview releases

--- haml/deckjs/helpers.rb
+++ haml/deckjs/helpers.rb
@@ -1,4 +1,5 @@
 require 'asciidoctor/backends/_stylesheets'
+#Asciidoctor::BASEBACKENDS['deckjs'] = 'html'
 # Add custom functions to this module that you want to use in your Haml
 # templates. Within the template you can invoke them as top-level functions
 # just like the built-in helper functions that Haml provides.
