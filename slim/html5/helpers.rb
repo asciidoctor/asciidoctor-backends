380be8e0e92b22d75a2a5226270c090e9dd024b8
Jakub Jirutka <jakub@jirutka.cz>
2014-10-09 02:20:26 +0200

    Fix slim/html5 to not render empty style tag; introduce 'style' helper

--- slim/html5/helpers.rb
+++ slim/html5/helpers.rb
@@ -3,6 +3,14 @@
 # (unless someone can show me how to include them in the evaluation context).
 # You can change the namespace to whatever you want.
 module Helpers
-  #def self.a_helper_function
-  #end
+
+  # Formats the given hash as CSS declarations for inline style.
+  def self.style_value(hash)
+    decls = []
+    hash.each do |prop, value|
+      prop = prop.to_s.gsub('_', '-')
+      decls << "#{prop}: #{value}" if value
+    end
+    decls.empty? ? nil : decls.join('; ') + ';'
+  end
 end
